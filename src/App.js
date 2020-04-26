be21a3837d541cf34a5dcc98391eb8fd7ebca6c6
Lokdev <lokdev@gmail.com>
2020-04-26 15:52:24 +0530

    Added Resume Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import {Route} from 'react-router-dom'
 import "./App.css";
 import Home from "./components/index";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Resume from "./components/Resume"
 
 function App() {
   return (
     <div>
       <CssBaseline />
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/resume" component={Resume} />
+     
     </div>
   );
 }
