14e8e19f6f11c867bffac7bbb29b21e45be17598
Lokdev <lokdev@gmail.com>
2020-04-26 13:19:41 +0530

    Initial commit - Project creation, Homepage creation

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+import Typed from "react-typed";
+import avatar from "../avatar.png";
+
+//CSS Styles
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title:{
+      color:"tomato"
+  },
+  subtitle:{
+      color:"tan",
+      marginBottom:"3rem"
+  },
+  typedContainer:{
+      position:"absolute",
+      top:"50%",
+      left:"50%",
+      transform:"translate(-50%,-50%)",
+      width: "100vw",
+      textAlign:"center",
+      zIndex:1
+  }
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.typedContainer}>
+    <Grid container justify ="center">
+    <Avatar className={classes.avatar} src={avatar} alt="Lokesh Dhanasekar" />
+    </Grid>
+     
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Lokesh Dhanasekar"]} typeSpeed={40} />
+      </Typography>
+      <br />
+      <Typography className={classes.subtitle} variant="h5">
+        <Typed
+          strings={["Spring Boot", "React JS", "ETL", "AWS"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
