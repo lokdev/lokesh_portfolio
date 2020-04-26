70280ba1c251998c85fcd64c0d011078b81c0e4c
Lokdev <lokdev@gmail.com>
2020-04-26 20:47:22 +0530

    Updates

---
+++ precache-manifest.ac2227fbc0973b36bb659aaede2163d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6654d5bced1c75979f2d2ba5910d33fa",
+    "url": "/lokesh/index.html"
+  },
+  {
+    "revision": "91a945975a067d70471e",
+    "url": "/lokesh/static/css/main.0f981bb4.chunk.css"
+  },
+  {
+    "revision": "2535cdfc496e4688f02d",
+    "url": "/lokesh/static/js/2.597475cc.chunk.js"
+  },
+  {
+    "revision": "e4279675a9c60179159c82343fcf3639",
+    "url": "/lokesh/static/js/2.597475cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a945975a067d70471e",
+    "url": "/lokesh/static/js/main.7a5e44be.chunk.js"
+  },
+  {
+    "revision": "82d1779240b9117f57a4",
+    "url": "/lokesh/static/js/runtime-main.7674a30c.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/lokesh/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/lokesh/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "11ee6301c320261afe3f0f83449edf30",
+    "url": "/lokesh/static/media/covid19.11ee6301.png"
+  },
+  {
+    "revision": "dee685944c29d81030ca11b4925d74b5",
+    "url": "/lokesh/static/media/resume.dee68594.jpg"
+  }
+]);
